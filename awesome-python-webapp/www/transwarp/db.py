6d9b9c3d05fc1b959da7635dc23727e00463b1d5
sunxiaoling <amylovesong.sun@gmail.com>
2015-09-22 22:19:41 +0800

    update db operation module

--- awesome-python-webapp/www/transwarp/db.py
+++ awesome-python-webapp/www/transwarp/db.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+__author__ = 'sunxiaoling'
+
 '''
 Database operation module.
 '''
@@ -12,7 +14,32 @@ import time, uuid, functools, threading, logging
 class Dict(dict):
 	'''
 	Simple dict but support access as x.y style.
-	TODO: doctest here
+
+	>>> d1 = Dict()
+	>>> d1['x'] = 100
+	>>> d1.x
+	100
+	>>> d1.y = 200
+	>>> d1['y']
+	200
+	>>> d2 = Dict(a=1, b=2, c='3')
+	>>> d2.c
+	'3'
+	>>> d2['empty']
+	Traceback (most recent call last):
+		...
+	KeyError: 'empty'
+	>>> d2.empty
+	Traceback (most recent call last):
+		...
+	AttributeError: 'Dict' object has no attribute 'empty'
+	>>> d3 = Dict(('a', 'b', 'c'),(1, 2, 3))
+	>>> d3.a
+	1
+	>>> d3.b
+	2
+	>>> d3.c
+	3
 	'''
 	
 	def __init__(self, names=(), values=(), **kw):
@@ -29,15 +56,58 @@ class Dict(dict):
 	def __setattr__(self, key, value):
 		self[key] = value
 
-class _Engine(object):
-	def __init__(self, connect):
-		self._connect = connect
-	def connect(self):
-		return self._connect()
-		
-engine = None
+def next_id(t=None):
+	'''
+	Return next id as 50-char string.
+
+	Args:
+		t: unix timestamp, default to None and using time.time().
+	'''
+	if t is None:
+		t = time.time()
+	return '%015d%s000' % (int(t * 1000), uuid.uuid4().hex)
+
+def _profiling(start, sql=''):
+	t = time.time() - start
+	if t > 0.1:
+		logging.warning('[PROFILING] [DB] %s: %s' % (t, sql))
+	else:
+		logging.info('[PROFILING] [DB] %s: %s' % (t, sql))
+
+class DBError(Exception):
+	pass
+
+class MultiColumnsError(DBError):
+	pass
+
+class _LasyConnection(object):
+	def __init__(self):
+		self.connection = None
+
+	def cursor(self):
+		if self.connection is None:
+			connection = engine.connect()
+			logging.info('open conection <%s>...' % hex(id(connection)))
+			self.connection = connection
+		return self.connection.cursor()
+
+	def commit(self):
+		self.connection.commit()
+
+	def rollback(self):
+		self.connection.rollback()
+
+	def cleanup(sefl):
+		if self.connection:
+			connection = self.connection
+			self.connection = None
+			logging.info('close connection <%s>...' % hex(id(connection)))
+			connection.close()
 
 class _DbCtx(threading.local):
+	'''
+	Thread local object that holds connection info.
+	'''
 	def __init__(self):
 		self.connection = None
 		self.transactions = 0
@@ -46,6 +116,7 @@ class _DbCtx(threading.local):
 		return not self.connection is None
 	
 	def init(self):
+		logging.info('open lazy connection...')
 		self.connection = _LasyConnection()
 		self.transactions = 0
 		
@@ -54,9 +125,24 @@ class _DbCtx(threading.local):
 		self.connection = None
 		
 	def cursor(self):
+		'''
+		Return cursor
+		'''
 		return self.connection.cursor()
-		
+
+# thread-local db context:
 _db_ctx = _DbCtx()
+
+# global engine object:
+engine = None
+
+class _Engine(object):
+
+	def __init__(self, connect):
+		self._connect = connect
+
+	def connect(self):
+		return self._connect()
 
 class _ConnectionCtx(object):
 	def __enter__(self):
