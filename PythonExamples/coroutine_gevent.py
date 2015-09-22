aee691d7ee5550af08f5806b6c4cdaf8493249a9
sunxiaoling <amylovesong.sun@gmail.com>
2015-09-22 21:04:36 +0800

    gevent exercise

--- PythonExamples/coroutine_gevent.py
+++ PythonExamples/coroutine_gevent.py
@@ -5,16 +5,39 @@
 
 __author__ = 'sunxiaoling'
 
-from gevent import monkey; monkey.patch_socket()
-import gevent
+def e_gevent():
+	from gevent import monkey; monkey.patch_socket()
+	import gevent
 
-def f(n):
-	for i in range(n):
-		print gevent.getcurrent(), i
+	def f(n):
+		for i in range(n):
+			print gevent.getcurrent(), i
+			gevent.sleep(0)
 
-g1 = gevent.spawn(f, 5)
-g2 = gevent.spawn(f, 5)
-g3 = gevent.spawn(f, 5)
-g1.join()
-g2.join()
-g3.join()
+	g1 = gevent.spawn(f, 5)
+	g2 = gevent.spawn(f, 5)
+	g3 = gevent.spawn(f, 5)
+	g1.join()
+	g2.join()
+	g3.join()
+
+def e_gevent_example():
+	from gevent import monkey; monkey.patch_all()
+	import gevent
+	import urllib2
+
+	def f(url):
+		print('GET: %s' % url)
+		resp = urllib2.urlopen(url)
+		data = resp.read()
+		print('%s bytes received from %s.' % (len(data), url))
+
+	gevent.joinall([
+		gevent.spawn(f, 'http://www.python.org/'),
+		gevent.spawn(f, 'https://www.yahoo.com/'),
+		gevent.spawn(f, 'https://github.com/'),
+	])
+
+if __name__ == '__main__':
+#	e_gevent()
+	e_gevent_example()
