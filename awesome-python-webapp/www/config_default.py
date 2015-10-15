ed138b8b872ec078be3a889440a9222799086d28
sunxiaoling <amylovesong.sun@gmail.com>
2015-10-15 17:24:22 +0800

    add configurations

---
+++ awesome-python-webapp/www/config_default.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'sunxiaoling'
+
+'''
+Default configurations.
+'''
+
+configs = {
+	'db': {
+		'host': '127.0.0.1',
+		'port': 3307,
+		'user': 'www-data',
+		'password': 'www-data',
+		'database': 'awesome'
+	},
+	'session': {
+		'secret': 'AwEsOmE'
+	}
+}
