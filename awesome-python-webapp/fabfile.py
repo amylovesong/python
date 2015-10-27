3dab042fb5257879866e3197047614affbbfd621
sunxiaoling <amylovesong.sun@gmail.com>
2015-10-27 18:38:51 +0800

    create fabfile

---
+++ awesome-python-webapp/fabfile.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'sunxiaoling'
+
+'''
+Deployment toolkit.
+'''
+
+import os, re
+
+from datetime import datetime
+from fabric.api import *
+
